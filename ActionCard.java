a60dcfee83a51846a95b9e9d6483641342db039d
mikolaslorant <lmikolas@itba.edu.ar>
2017-06-14 11:54:05 -0300

    Create ActionCard.java

--- ActionCard.java
+++ ActionCard.java
@@ -2,30 +2,19 @@
  * Created by lmikolas on 07/06/17.
  */
 public class ActionCard extends Card{
-    Behaviour[] behaviours;
-    private int behaviourCounter=0;
-
-    ActionCard(String name, Integer score, String color, Behaviour...behaviours){
+    private Behaviour behaviour;
+    ActionCard(String name, Integer score, String color, Behaviour behaviour){
         super(name, score, color);
-        this.behaviours = behaviours;
+        this.behaviour = behaviour;
     }
 
 
     public void makeAction(String ... args) {
-            int k = 0;
-            if(args.length == k){
-                behaviours[k].action(args);
-            }
-            else if(args.length == 1){
-                if(behaviours.length != 2)
-                    throw new UnsupportedOperationException("Not enough arguemnts for behaviour")
-                behaviours[1].action(args);
-            }
+            behaviour.action(args);
+           
     }
 
-    public void setColor(String color){
-        if(behaviours[behaviourCounter].setColor())
-    }
+   
 
 
     public boolean match(Card card){
