3ddde1fce479035d705556700b4d9843fbcf155d
JuanPabloLoCoco <jlococo@itba.edu.ar>
2017-06-14 12:29:29 -0300

    Create DrawPile.java
    Agregue el crear el mazo. Se insertan dos copias de cada carta, y 4 copias de las especiales. Se agrega metodo privado getDrawPile();

--- DrawPile.java
+++ DrawPile.java
@@ -17,24 +17,43 @@ public class DrawPile {
     }
 
     private int size(){
-        return drawPile.size();
+        return getDrawPile().size();
     }
 
     private void shuffle(){
-        Collections.shuffle(drawPile);
+        Collections.shuffle(getDrawPile());
     }
 
     public Card getCard() {
-        return drawPile.remove(size()-1);
+        return getDrawPile().remove(size()-1);
+    }
+    
+    private ArrayList<Card> getDrawPile(){
+      return this.drawPile;
     }
 
     public void newDeck() {
-        // Crea el mazo de cartas
-           shuffle();
+        String[] cardName = Card.getCardNames();
+        String[] cardColors = Card.getCardColors();
+        ArrayList<Card> cardList = new ArrayList<>();
+        for (int color = 1;color < cardColors.length;color++ ){
+            for (int name = 0; name < cardName.length-3;name++){
+            getDrawPile().add(CardFactory.getCard(color,name));
+            getDrawPile().add(CardFactory.getCard(color,name));
+            }
+        }
+        for (int count = 0; count < 4;count++ ){
+            getDrawPile().add(CardFactory.getCard("black","Wild"));
+            getDrawPile().add(CardFactory.getCard("black","DrawFour")); 
+            getDrawPile().add(CardFactory.getCard("black","Mirror"));
+        }
+        shuffle();
     }
 
     public void setDrawPile(ArrayList<Card> cardArray){
-        drawPile=cardArray;
+        getDrawPile()=cardArray;
         shuffle();
     }
+    
+    
 }
