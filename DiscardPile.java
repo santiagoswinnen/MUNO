d999de86ee78a9b5af32ad276c5d83cf69e94ea3
JuanPabloLoCoco <jlococo@itba.edu.ar>
2017-06-16 20:04:12 -0300

    Update DiscardPile.java

--- DiscardPile.java
+++ DiscardPile.java
@@ -16,22 +16,32 @@ public class DiscardPile {
         return getDiscardPile().get(size() - 1);
     }
 
+    /**
+     * Agrega la carta al mazo de descarte.
+     * Si la ultima carta era wildcard, le retorna su color a "black"
+     * @param card Carta a agregar al mazo.
+     */
     public void throwCard(Card card){
         if (!isEmpty() && lastCard().isWildCard()){
             lastCard().setColor("black");
         }
         getDiscardPile().add(card);
     }
-    
+
     private ArrayList<Card> getDiscardPile(){
         return this.discardPile;
     }
-    
+
     private int size(){
         return getDiscardPile().size();
     }
 
-    // Una vez que una WildCard (Carta color negro) deja de estar primera, le cambia el color a negro.
+    /**
+     *
+     * @return Un array list de Cards, con las cartas
+     *          que se encontraban en el DiscardPile menos
+     *          la ultima carta.
+     */
     public ArrayList<Card> askCards(){
         Card cardAux= lastCard();
         ArrayList <Card> aux = new ArrayList<Card>();
@@ -42,9 +52,9 @@ public class DiscardPile {
         getDiscardPile().add(cardAux);
         return aux;
     }
-    
+
     public boolean isEmpty(){
-    	return this.discardPile.size() == 0;
+        return this.discardPile.size() == 0;
     }
 
 }
